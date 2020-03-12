2eb78595c84228b8f44f0e8b98ab602aeee84118
wangjie_fourth <wangjie_fourth@163.com>
2020-03-12 16:47:57 +0800

    [add]：添加相关测试

---
+++ src/test/java/com/wangjie/supershop/service/TelVerificationServiceTest.java
@@ -0,0 +1,22 @@
+package com.wangjie.supershop.service;
+
+import com.wangjie.supershop.controller.AuthController;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TelVerificationServiceTest {
+    public static AuthController.TelAndCode VALID_PARAMETER = new AuthController.TelAndCode("138123456789", null);
+    public static AuthController.TelAndCode NULL_PARAMETER = new AuthController.TelAndCode(null, null);
+
+
+    @Test
+    public void returnTrueIfValid() {
+        Assertions.assertTrue(new TelVerificationService().verifyTelParameter(VALID_PARAMETER));
+    }
+
+    @Test
+    public void returnFalseIfNull() {
+        Assertions.assertFalse(new TelVerificationService().verifyTelParameter(NULL_PARAMETER));
+        Assertions.assertFalse(new TelVerificationService().verifyTelParameter(null));
+    }
+}
