e2d624d396ee16d724ef2e65edb84f596d1c8b5a
wangjie_fourth <wangjie_fourth@163.com>
2020-03-20 14:57:30 +0800

    [update]：添加USER表字段注释

--- src/main/resources/db/migration/V1__CreateUser.sql
+++ src/main/resources/db/migration/V1__CreateUser.sql
@@ -1,8 +1,8 @@
 create table USER(
-    ID         bigint primary key auto_increment,
-    NAME       varchar(100),
-    TEL        varchar(40) unique ,
-    AVATAR_URL varchar(1024),
-    CREATED_AT timestamp default current_timestamp,
-    UPDATED_AT timestamp default current_timestamp on update current_timestamp
+    ID         bigint primary key auto_increment comment '主键id',
+    NAME       varchar(100) comment '用户名称',
+    TEL        varchar(40) unique comment '电话号码',
+    AVATAR_URL varchar(1024) comment '用户头像地址',
+    CREATED_AT timestamp default current_timestamp comment '创建时间',
+    UPDATED_AT timestamp default current_timestamp on update current_timestamp comment '上次更新时间'
 );
